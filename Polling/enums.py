5568b6bed4f852c5603b749ffb893d76042d9eb7
Mahdi Abbasi <ma.abbasi.9675@gmail.com>
2018-12-21 11:38:24 +0330

    Add base models

---
+++ Polling/enums.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class PollStatus(Enum):
+    IN_PROGRESS = 0
+    CLOSED = 1
---
+++ Polling/models.py
@@ -0,0 +1,27 @@
+from django.contrib.postgres.fields import JSONField
+from django.db import models
+
+from Polling.enums import PollStatus
+
+
+class User(models.Model):
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(unique=True)
+
+
+class Poll(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(max_length=2048, null=True, blank=True)
+    status = models.PositiveSmallIntegerField(choices=[(st, st.value) for st in PollStatus],
+                                              default=PollStatus.IN_PROGRESS)
+
+
+class PollOption(models.Model):
+    poll = models.ForeignKey(Poll)
+    value = models.CharField(max_length=255)
+
+
+class UserPoll(models.Model):
+    user = models.ForeignKey(User)
+    poll = models.ForeignKey(Poll)
+    choices = JSONField()
