7f2324c2d56a298b858829d29a5b3ef150f28502
Mahdi Abbasi <ma.abbasi.9675@gmail.com>
2018-12-23 13:32:18 +0330

    Add admin panel

--- PenguinBackend/urls.py
+++ PenguinBackend/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    # url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls')),
     url(r'^api/v1/polling/', include('Polling.urls')),
 ]
--- Polling/admin.py
+++ Polling/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from Polling.models import Poll
+
+
+class PollAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'title', 'description', 'status')
+
+
+admin.site.register(Poll, PollAdmin)
