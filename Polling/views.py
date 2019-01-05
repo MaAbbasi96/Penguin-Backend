f9572d4d228d86875fdd60c641d3fb12adddb568
AMK6610 <amir.karimi6610@gmail.com>
2019-01-06 00:30:15 +0330

    fix: message emptiness

--- Polling/views.py
+++ Polling/views.py
@@ -20,6 +20,8 @@ class PollManagementView(ViewSet):
         participants = wrapper.get_body_param('participants')
         is_normal = wrapper.get_body_param('is_normal')
         message = wrapper.get_body_param('message')
+        if not message:
+            message = None
         user = get_object_or_404(User, username=username)
         PollingServices().create_poll(title, description, user, options, participants, is_normal, message)
         return Response(status=status.HTTP_200_OK)
@@ -28,6 +30,8 @@ class PollManagementView(ViewSet):
         wrapper = RequestWrapper(request)
         poll = get_object_or_404(Poll, id=poll_id)
         message = wrapper.get_body_param('message')
+        if not message:
+            message = None
         PollingServices().edit_poll(poll, message)
         return Response(status=status.HTTP_200_OK)
 
